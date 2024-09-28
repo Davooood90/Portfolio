03012aad811ba7ff70ad5844585becf5f42edb1f
Davooood90 <davooood90@gmail.com>
2024-09-27 20:18:58 -0700

    Fixed modal closer

--- static/js/main.js
+++ static/js/main.js
@@ -18,12 +18,26 @@ function navbarShrink () {
 function openModal (selectedModal) {
     let modal = document.getElementById(selectedModal);
     modal.style.display = "flex";
-};
+
+    document.body.style.overflow = 'hidden';
+}
 
 function closeModal (selectedModal) {
     let modal = document.getElementById(selectedModal);
     modal.style.display = "none";
+    document.body.style.overflow = 'initial';
 };
 
+let detect = document.getElementsByName("detect");
 
-     
+window.onclick = function(event) {
+    if (event.target.id == "cocoModal") {
+        closeModal("cocoModal");
+    } else if (event.target.id == "valeModal") {
+        closeModal("valeModal");
+    } else if (event.target.id == "botModal") {
+        closeModal("botModal");
+    } else if (event.target.id == "osaModal") {
+        closeModal("osaModal");
+    }
+  }
