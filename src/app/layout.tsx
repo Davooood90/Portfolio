b087d1835acd026265c3f345b082998ed86cebaa
Davooood90 <davooood90@gmail.com>
2025-05-08 09:56:10 -0700

    Added Website Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@ import Navbar from "../components/navbar";
 import "./index.css";
 import Footer from "../components/footer";
 import React, { Suspense } from "react";
+import Head from "next/head";
 
 export const metadata = {
   title: "David's Portfolio",
   description: "Hi! My name is David and welcome to my corner of the internet!",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
