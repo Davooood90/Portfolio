517947e98bef80b2c6d37d2e960b3f1fbb1d5ddd
Davooood90 <davooood90@gmail.com>
2026-08-14 10:23:57 -0700

    reset repo to Next.js template

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import TitlePage from "../components/title";
-
-export default function NotFound() {
-  return (
-    <div className="custom-gradient">
-      <TitlePage title="Page Not Found" />
-    </div>
-  );
-}
