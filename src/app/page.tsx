a70875daf7c631e329188eb6715fb6d5e614aafd
Davooood90 <davooood90@gmail.com>
2026-08-16 15:49:37 -0700

    set up theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { setTheme } = useTheme();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -49,6 +54,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <button onClick={() => setTheme("light")}>light</button>
+          <button onClick={() => setTheme("dark")}>dark</button>
+          <button onClick={() => setTheme("system")}>system</button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
